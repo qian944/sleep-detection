1b24b30aac0b6fe2e19ef6bb9914fccf4828977a
qian944 <qianxunliu48@gmail.com>
2025-06-08 10:34:49 +0800

    Update model.py

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ def generate_gradcam(model, input_tensor, device):
     def backward_hook(module, grad_input, grad_output):
         gradients.append(grad_output[0])
 
-    target_layer = model.backbone.layer4[-1]
+    target_layer = model.features[7][-1]
     fh = target_layer.register_forward_hook(forward_hook)
     bh = target_layer.register_backward_hook(backward_hook)
 
