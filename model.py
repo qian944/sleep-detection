35930b1973ace6594ae86289edb853fb23eb3e80
qian944 <qianxunliu48@gmail.com>
2025-06-08 09:14:12 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,23 @@
 import torch
-import torch.nn as nn
-import torchvision.models as models
+from model_cbam import ResNet18_CBAM
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import RegressionTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
-class SRSSModel(nn.Module):
-    def __init__(self):
-        super(SRSSModel, self).__init__()
-        self.backbone = models.resnet18(pretrained=False)
-        # 修改最后全连接层输出为1个回归值
-        self.backbone.fc = nn.Linear(self.backbone.fc.in_features, 1)
+def load_model_from_hf():
+    model = ResNet18_CBAM(pretrained=False)
+    checkpoint = torch.hub.load_state_dict_from_url(
+        'https://huggingface.co/qianxunliu/sleep-detection/resolve/main/model.pt',
+        map_location=torch.device('cpu'),
+        file_name='model.pt'
+    )
+    model.load_state_dict(checkpoint)
+    model.eval()
+    return model
 
-    def forward(self, x):
-        return self.backbone(x).squeeze(1)  # 返回形状 (batch,)
+def get_cam(model, input_tensor):
+    target_layers = [model.features[-4]]  # 最后一个 CBAM 前的 layer4 输出
+    cam = GradCAM(model=model, target_layers=target_layers, use_cuda=False)
+    targets = [RegressionTarget(0)]
+    grayscale_cam = cam(input_tensor=input_tensor, targets=targets)[0]
+    return grayscale_cam
